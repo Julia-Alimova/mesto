21085be0172f832d1e03362ee75cc4912b66144c
Julia Alimova <ju.vorobewa@yandex.ru>
2022-06-01 10:22:07 +0300

    добавить загрузку карточек из js, попап добавления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,99 @@
-const editButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
+// Карточки
+const cards = document.querySelector('.cards');
+const cardContent = document.querySelector('.card').content;
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function(element) {
+  const cardElement = cardContent.cloneNode(true);
+  cardElement.querySelector('.card__photo').src = element.link;
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cards.append(cardElement)
+});
+
+
+const editButton = document.querySelector('.profile__edit-button');                       // Форма редактирования
+const editPopup = document.querySelector('.popup_type_edit');
 const userName = document.querySelector('.profile__name');
 const userInfo = document.querySelector('.profile__description');
-const popupCloseButton = document.querySelector('.form__button_type_exit');
+const editPopupCloseButton = editPopup.querySelector('.form__button_type_exit');
 const nameFieldElement = document.querySelector('.form__item_el_name');
 const infoFieldElement = document.querySelector('.form__item_el_description');
-const formElement = document.querySelector('.form');
+const editFormElement = document.querySelector('.edit-form');
+const addPopup = document.querySelector('.popup_type_add');                              // Форма добавления
+const addButton = document.querySelector('.profile__add-button');
+const addPopupCloseButton = addPopup.querySelector('.form__button_type_exit')
+const addFormElement = document.querySelector('.add-form')
 
-function openPopup() {
-  popup.classList.add('popup_opened')
+// Открытие попапов
+function openPopup(poupElement) {
+  poupElement.classList.add('popup_opened')
+}
+
+editButton.addEventListener('click', () => {
+  openPopup(editPopup);
   nameFieldElement.value = userName.textContent;
   infoFieldElement.value = userInfo.textContent;
+});
+
+addButton.addEventListener('click', () => {
+  openPopup(addPopup);
+});
+
+// Закрытие попапов
+
+function closePopup(poupElement) {
+  poupElement.classList.remove('popup_opened')
 }
 
-function closePopup() {
-  popup.classList.remove('popup_opened')
+editPopupCloseButton.addEventListener('click', () => {
+  closePopup(editPopup)
+});
+
+addPopupCloseButton.addEventListener('click', () => {
+  closePopup(addPopup)
+})
+
+// Сабмиты
+function submitPopup(evt) {
+  evt.preventDefault()
 }
 
-function submitPopup(event) {
-  event.preventDefault()
+editFormElement.addEventListener('submit', () => {
+  submitPopup
   userName.textContent = nameFieldElement.value;
   userInfo.textContent = infoFieldElement.value;
-  closePopup()
-}
+  closePopup(editPopup)
+});
 
-editButton.addEventListener('click', openPopup)
-popupCloseButton.addEventListener('click', closePopup)
-formElement.addEventListener('submit', submitPopup)
+// Сабмит для второй формы - нужен?
+
+editFormElement.addEventListener('submit', () => {
+  submitPopup
+  closePopup(addPopup)
+});
